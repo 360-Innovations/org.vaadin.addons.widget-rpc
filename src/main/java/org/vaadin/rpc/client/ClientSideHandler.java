fadb1e9192e94e383b84759b33b81d12cb1c80f4
Eric Belanger <ebelanger@360-innovations.com>
2012-01-11 14:33:06 -0500

    applied Apache License 2.0

--- src/main/java/org/vaadin/rpc/client/ClientSideHandler.java
+++ src/main/java/org/vaadin/rpc/client/ClientSideHandler.java
@@ -1,34 +1,50 @@
+/*
+ *   Copyright 2011 Sami Ekblad sami@vaadin.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *   
+ *   NOTICE: modified by Eric Belanger ebelanger@360-innovations.com
+ */
 package org.vaadin.rpc.client;
 
 import java.io.Serializable;
 
 /**
  * Interface for receiving / handling server-side calls.
- *
+ * 
  * @author Sami Ekblad / Vaadin
- *
+ * 
  */
-public interface ClientSideHandler extends Serializable{
+public interface ClientSideHandler extends Serializable
+{
 
-    /**
-     * Invoked when client-side widget should be initialized.
-     *
-     * If an asynchronous init is made the handler must call the initComplete
-     * method to notify about the it.
-     *
-     * @param params
-     *            Initialization parameters sent from the server.
-     * @return true if init will be made asynchronously, false if init is
-     *         complete already,
-     */
-    public boolean initWidget(Object[] params);
+	/**
+	 * Invoked when client-side widget should be initialized.
+	 * 
+	 * If an asynchronous init is made the handler must call the initComplete method to notify about the it.
+	 * 
+	 * @param params
+	 *            Initialization parameters sent from the server.
+	 * @return true if init will be made asynchronously, false if init is complete already,
+	 */
+	public boolean initWidget(Object[] params);
 
-    /**
-     * Handle an otherwise unhandled call from the server.
-     *
-     * @param method
-     * @param params
-     */
-    public void handleCallFromServer(String method, Object[] params);
+	/**
+	 * Handle an otherwise unhandled call from the server.
+	 * 
+	 * @param method
+	 * @param params
+	 */
+	public void handleCallFromServer(String method, Object[] params);
 
 }
--- src/main/java/org/vaadin/rpc/shared/Method.java
+++ src/main/java/org/vaadin/rpc/shared/Method.java
@@ -1,23 +1,40 @@
+/*
+ *   Copyright 2011 Sami Ekblad sami@vaadin.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *   
+ *   NOTICE: modified by Eric Belanger ebelanger@360-innovations.com
+ */
 package org.vaadin.rpc.shared;
 
 import java.io.Serializable;
 
 /**
- * Generic method handler interface. This interface is used both client and
- * server side to receive / handle remote calls.
- *
+ * Generic method handler interface. This interface is used both client and server side to receive / handle remote calls.
+ * 
  * @author Sami Ekblad / Vaadin
- *
+ * 
  */
-public interface Method extends Serializable{
+public interface Method extends Serializable
+{
 
-    /**
-     * Invoke a method by name.
-     *
-     * @param methodName
-     *            name of the methd to invoke.
-     * @param params
-     *            Array of untyped parameters.
-     */
-    void invoke(String methodName, Object[] params);
+	/**
+	 * Invoke a method by name.
+	 * 
+	 * @param methodName
+	 *            name of the methd to invoke.
+	 * @param params
+	 *            Array of untyped parameters.
+	 */
+	void invoke(String methodName, Object[] params);
 }
