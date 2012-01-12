f4882ea43957fd7703437dfae2a0afdea5e55db2
Eric Belanger <ebelanger@360-innovations.com>
2012-01-12 15:26:09 -0500

    changed licence information and increased major version since api is not compatible.

--- src/main/java/org/vaadin/rpc/shared/Method.java
+++ src/main/java/org/vaadin/rpc/shared/Method.java
@@ -13,7 +13,6 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  *   
- *   NOTICE: modified by Eric Belanger ebelanger@360-innovations.com
  */
 package org.vaadin.rpc.shared;
 
